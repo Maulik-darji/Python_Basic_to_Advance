bfe605236ee1695f5750eb5d8db9b0c624f13634
Maulik Darji <maulik.darji2005@gmail.com>
2025-12-13 07:47:12 +0530

    Initial Commit

---
+++ Numbers/1_Basic_Number_Patterns/1_Natural_number.py
@@ -0,0 +1,4 @@
+n = int(input("Enter Size: "))
+print("Natural Numbers")
+for i in range(n):
+    print(i+1)
---
+++ Numbers/1_Basic_Number_Patterns/2_odd_Even.py
@@ -0,0 +1,5 @@
+n = int(input("Enter Number to check odd or even: "))
+if n%2 == 0:
+    print("Number: ",n," Even")
+else:
+    print("Number: ",n," Odd")
