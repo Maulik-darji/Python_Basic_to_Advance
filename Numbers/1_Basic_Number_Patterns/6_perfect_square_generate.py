20c7f005e96418ca4639ab7c691d9d279372c20f
Maulik Darji <maulik.darji2005@gmail.com>
2025-12-20 07:47:13 +0530

    Python Day 2

---
+++ Numbers/1_Basic_Number_Patterns/6_perfect_square_generate.py
@@ -0,0 +1,3 @@
+n = int(input("Enter Number To Generrate  Perfect Square: "))
+condition = n*n
+print(f"Perfect Square of {n} :",condition  )
---
+++ Numbers/1_Basic_Number_Patterns/7_perfect_cube_check.py
@@ -0,0 +1,8 @@
+import math
+n = int(input("Enter Number to Check if Perfect Cube: "))
+x = int(math.cbrt(n))
+condition = x*x*x
+if n == condition:
+    print("Perfect Cube")
+else:
+    print("Not Perfect Cube")
