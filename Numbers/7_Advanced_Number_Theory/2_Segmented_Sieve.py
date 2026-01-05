d3eefb71185ed1fafbb57a05cc3f0c0551110739
Maulik Darji <maulik.darji2005@gmail.com>
2026-01-05 06:03:58 +0530

    Python Day 15

--- Numbers/7_Advanced_Number_Theory/2_Segmented_Sieve.py
+++ Numbers/7_Advanced_Number_Theory/2_Segmented_Sieve.py
@@ -2,13 +2,13 @@ def seg_siv(L,R):
     segment=[]
     for i in range(L, R+1):
         segment.append(i)
+    if 1 in segment:
+        segment.remove(1)
     for i in range(2, int(R**0.5)+1):
         for j in range(i*2, R+1,i):
             if j in segment:
                 segment.remove(j)
-        if j in segment:
-            segment.remove(1)
-        return segment
+    return segment
 L = int(input("Enter L: "))
 R = int(input("Enter R: "))
 print("Prime Numbers In Range: ")
